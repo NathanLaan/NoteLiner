692ed4814451497997151cac42d90640117b8898
Nathan Laan <nathanlaan@gmail.com>
2013-08-20 14:50:25 -0600

    Working on SQL for Note.

--- NoteLiner.App/Note.cs
+++ NoteLiner.App/Note.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Data.SQLite;
 
 namespace NoteLiner.App
 {
@@ -9,10 +7,49 @@ namespace NoteLiner.App
     {
 
         public bool Dirty { get; set; }
+        public int ID { get; set; }
         public string Title { get; set; }
         public string Contents { get; set; }
         public DateTime Created { get; set; }
         public DateTime Updated { get; set; }
+
+
+        #region SQL
+        
+        private static readonly string SQL_INSERT
+            = "INSERT INTO [Note] Title,Contents,Created,Updated"
+            + " VALUES(@Title,@Contents,@Created,@Updated);"
+            + " SELECT last_insert_rowid();";
+        private static readonly string SQL_UPDATE
+            = "UPDATE [Note] SET Title=@Title, Contents=@Contents, Created=@Created, Updated=@Updated"
+            + " WHERE ID=@ID;";
+        
+        #endregion
+
+
+        public void Save(SQLiteConnection sqliteConnection)
+        {
+            sqliteConnection.Open();
+            SQLiteCommand sqlCommand = new SQLiteCommand(SQL_INSERT, sqliteConnection);
+            sqlCommand.Parameters.AddWithValue("@ID", ID);
+            sqlCommand.Parameters.AddWithValue("@Title", Title);
+            sqlCommand.Parameters.AddWithValue("@Contents", Contents);
+            sqlCommand.Parameters.AddWithValue("@Created", Created);
+            sqlCommand.Parameters.AddWithValue("@Updated", Updated);
+            sqlCommand.ExecuteNonQuery();
+        }
+
+        public void Insert(SQLiteConnection sqliteConnection)
+        {
+            sqliteConnection.Open();
+            SQLiteCommand sqlCommand = new SQLiteCommand(SQL_INSERT, sqliteConnection);
+            sqlCommand.Parameters.AddWithValue("@Title", Title);
+            sqlCommand.Parameters.AddWithValue("@Contents", Contents);
+            sqlCommand.Parameters.AddWithValue("@Created", Created);
+            sqlCommand.Parameters.AddWithValue("@Updated", Updated);
+            object returnValue = sqlCommand.ExecuteScalar();
+            this.ID = int.Parse(returnValue.ToString());
+        }
 
     }
 }
--- NoteLiner.App/NoteList.cs
+++ NoteLiner.App/NoteList.cs
@@ -1,14 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SQLite;
 
 namespace NoteLiner.App
 {
     internal class NoteList
     {
 
+        public NoteList()
+        {
+            this.List = new List<Note>();
+        }
+
         public List<Note> List { get; set; }
 
         public string FilePath { get; set; }
+
+        private string ConnectionString
+        {
+            get
+            {
+                return string.Format("Data Source={0}", this.FilePath);
+            }
+        }
 
         public void Load()
         {
@@ -16,18 +30,34 @@ namespace NoteLiner.App
 
         public void Save()
         {
+
+            // TODO: open DB connection
+
+            using (SQLiteConnection sqliteConnection = new SQLiteConnection(this.ConnectionString))
+            {
+
+                foreach (Note note in this.List)
+                {
+                    if (note.Dirty)
+                    {
+                        //
+                        // TODO: Save
+                        //
+                    }
+                }
+            }
         }
 
 
 
-        private string CreateTableNote()
+        private string CreateTableSQL_Note()
         {
             string s = "CREATE TABLE [Note] ("
-                        + "[ID] INTEGER  NOT NULL PRIMARY KEY AUTOINCREMENT,"
-                        + "[Title] VARCHAR(300)  UNIQUE NOT NULL,"
-                        + "[Contents] TEXT  NULL,"
-                        + "[Created] TIMESTAMP  NULL,"
-                        + "[Updated] TIMESTAMP  NULL"
+                        + "[ID] INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,"
+                        + "[Title] VARCHAR(300) UNIQUE NOT NULL,"
+                        + "[Contents] TEXT NULL,"
+                        + "[Created] TIMESTAMP NULL,"
+                        + "[Updated] TIMESTAMP NULL"
                         + ")";
             return s;
         }
