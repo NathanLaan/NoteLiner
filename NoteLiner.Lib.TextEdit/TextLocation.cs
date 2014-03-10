b92c85170aa01b5980258ba202ef991effd1e786
Nathan Laan <nathanlaan@gmail.com>
2014-03-10 15:36:11 -0600

    Added TextLocation and Notes (TODO) file.

---
+++ NoteLiner.Lib.TextEdit/TextLocation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoteLiner.Lib.TextEdit
+{
+    public class TextLocation
+    {
+
+        public int LineNumber { get; set; }
+        public int CharNumber { get; set; }
+        
+        public TextLocation(int lineNumber, int charNumber)
+        {
+            this.LineNumber = lineNumber;
+            this.CharNumber = charNumber;
+        }
+
+    }
+
+}
