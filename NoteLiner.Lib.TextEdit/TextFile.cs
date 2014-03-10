d2b325fa28deb6449d4f1dce0abf947e42243cf5
Nathan Laan <nathanlaan@gmail.com>
2014-03-10 16:01:18 -0600

    Working on TextFile code.

---
+++ NoteLiner.Lib.TextEdit/TextFile.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoteLiner.Lib.TextEdit
+{
+    public class TextFile
+    {
+
+        private readonly List<TextLine> textLineList = new List<TextLine>();
+
+        public TextEditControl Control { get; set; }
+
+
+        public event EventHandler<EventArgs> LineInserted;
+        public event EventHandler<EventArgs> LineRemoved;
+        public event EventHandler<EventArgs> TextChanging;
+        public event EventHandler<EventArgs> TextChanged;
+
+
+        public TextFile()
+        {
+        }
+
+
+    }
+}
