62d118b62801dfa4acb79b01823d2d4ebf4d6e38
Nathan Laan <nathanlaan@gmail.com>
2014-04-24 15:37:13 -0600

    Added IExceptionManager.

--- NoteLiner.Lib.TextEdit/Command/CommandManager.cs
+++ NoteLiner.Lib.TextEdit/Command/CommandManager.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using NoteLiner.Lib.TextEdit.Ex;
 
 namespace NoteLiner.Lib.TextEdit.Command
 {
     public class CommandManager
     {
+
+        public IExceptionManager ExceptionManager { get; set; }
 
         private Stack<CommandBase> undoStack;
         private Stack<CommandBase> redoStack;
@@ -19,7 +22,18 @@ namespace NoteLiner.Lib.TextEdit.Command
         public void ExecuteCommand(CommandBase command)
         {
             this.undoStack.Push(command);
-            command.Execute();
+            try
+            {
+                command.Execute();
+            }
+            catch (Exception e)
+            {
+                this.undoStack.Pop();
+                if (this.ExceptionManager != null)
+                {
+                    this.ExceptionManager.HandleException(e);
+                }
+            }
         }
 
 
---
+++ NoteLiner.Lib.TextEdit/Ex/IExceptionManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NoteLiner.Lib.TextEdit.Ex
+{
+    public interface IExceptionManager
+    {
+
+        void HandleException(Exception e);
+
+    }
+}
